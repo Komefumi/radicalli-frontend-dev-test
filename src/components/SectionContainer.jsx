cf81a35fe765fc56f9d157341a2355e7680c001c
Komefumi <arjunraju.pillai.1997@gmail.com>
2021-03-04 08:53:36 +0530

    Some styling for Section Container

--- src/components/SectionContainer.jsx
+++ src/components/SectionContainer.jsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 const SectionContainer = styled.section`
   text-align: left;
+  background-color: #fefefe;
+  padding: 3em;
 `;
 
 export default SectionContainer;
