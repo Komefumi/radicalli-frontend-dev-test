d20ecc2d32c4a48a6aea6ba5bbd3866f82bf32da
Komefumi <arjunraju.pillai.1997@gmail.com>
2021-03-04 08:50:34 +0530

    Make use of ids in todo mock data

--- src/mock_data/todos.js
+++ src/mock_data/todos.js
@@ -1,4 +1,6 @@
 import faker from 'faker';
+import { nanoid } from 'nanoid';
+
 import { statuses } from '../constants';
 
 const pickStatus = () => statuses[Math.round(Math.random() * 10) % 4];
@@ -7,6 +9,7 @@ const MockTodos = () => {
   const builtTodos = Array(10)
     .fill(10)
     .map(() => ({
+      id: nanoid(),
       title: faker.vehicle.manufacturer(),
       description: faker.lorem.sentence(),
       dueDate: faker.date.past(),
