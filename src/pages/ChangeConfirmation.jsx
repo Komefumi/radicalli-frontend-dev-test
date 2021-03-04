0bd5e1ac88a452c4c9e5047d42f1d11cd326f25b
Komefumi <arjunraju.pillai.1997@gmail.com>
2021-03-04 08:53:45 +0530

    Fleshed out Change Confirmation Page

--- src/pages/ChangeConfirmation.jsx
+++ src/pages/ChangeConfirmation.jsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { PrimaryButton } from '@fluentui/react';
+import styled from 'styled-components';
 
 import PageContainer from '../components/PageContainer';
+import SectionContainer from '../components/SectionContainer';
+import SectionTitle from '../components/SectionTitle';
+import TodoForm from '../components/TodoForm';
+import { CONFIRM_EDIT, RESET_EDITING } from '../constants';
 
-function ChangeConfirmation() {
-  return <PageContainer>This is the change confirmation page</PageContainer>;
+const ButtonContainer = styled.section`
+  margin-top: 2em;
+  display: block;
+`;
+
+function ChangeConfirmation({ editing, confirmEdit, resetEditing }) {
+  const history = useHistory();
+  useEffect(() => {
+    if (editing === null) {
+      history.goBack();
+    }
+
+    return () => {};
+  }, [editing, history]);
+
+  const signalEditingEnd = () => {
+    resetEditing();
+  };
+
+  return (
+    editing && (
+      <PageContainer>
+        <SectionContainer>
+          <SectionTitle>Confirm Edit</SectionTitle>
+
+          <TodoForm {...editing} readOnly onSubmission={confirmEdit} />
+
+          <ButtonContainer>
+            <PrimaryButton text='Return' onClick={signalEditingEnd} />
+          </ButtonContainer>
+        </SectionContainer>
+      </PageContainer>
+    )
+  );
 }
 
-export default ChangeConfirmation;
+const ChangeConfirmationConnected = connect(
+  (state) => {
+    const {
+      todoState: { editing },
+    } = state;
+    return {
+      editing: editing,
+    };
+  },
+  (dispatch) => {
+    return {
+      confirmEdit: () => {
+        dispatch({ type: CONFIRM_EDIT });
+      },
+      resetEditing: () => {
+        dispatch({ type: RESET_EDITING });
+      },
+    };
+  }
+)(ChangeConfirmation);
+
+export default ChangeConfirmationConnected;
