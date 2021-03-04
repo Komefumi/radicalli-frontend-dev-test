e108ce42fcfd0760fd133f861cc2352b200d9489
Komefumi <arjunraju.pillai.1997@gmail.com>
2021-03-04 16:33:52 +0530

    Make use of HashRouter for Github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { Toggle } from '@fluentui/react';
 import styled from 'styled-components';
 import { Provider, useDispatch } from 'react-redux';
